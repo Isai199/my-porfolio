313f85760726d72ce426266d21745ff53984be91
Isai199 <h_isai_gomez@hotmail.com>
2024-07-16 20:16:54 -0600

    UPDATE(popup): agregue el codigo base, para crear un custom elelemt de un modal

--- my-porfolio/src/app/app.component.ts
+++ my-porfolio/src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { createCustomElement } from "@angular/elements";
+import { PopupComponent } from "./popup/popup.component";
+import { PopupService } from "./popup.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
-  templateUrl: './app.component.html',
+  providers: [PopupService],
+  imports: [RouterOutlet, PopupComponent],
+  //templateUrl: './app.component.html',
+  template: `
+    <input #input value="Message" />
+    <button type="button" (click)="popup.showAsComponent(input.value)">Show as component</button>
+    <button type="button" (click)="popup.showAsElement(input.value)">Show as element</button>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-porfolio';
+
+  constructor(
+    injector: Injector,
+    public popup: PopupService
+  ) {
+    const PopupElement = createCustomElement(PopupComponent, {injector});
+
+    customElements.define('popup-element', PopupElement);
+  }
 }
---
+++ my-porfolio/src/app/popup.service.ts
@@ -0,0 +1,44 @@
+import { ApplicationRef, createComponent, EnvironmentInjector, Injectable } from '@angular/core';
+import {NgElement, WithProperties} from '@angular/elements';
+import { PopupComponent } from "./popup/popup.component";
+
+@Injectable()
+export class PopupService {
+
+  constructor(
+    private injector: EnvironmentInjector,
+    private ApplicationRef: ApplicationRef
+  ) {}
+
+  // antes de agregar al dom, se define la estructra del popup
+  showAsComponent(message: string) {
+    const popup = document.createElement('popup-component');
+
+    const popupComponentRef = createComponent(PopupComponent, {
+      environmentInjector: this.injector,
+      hostElement: popup
+    });
+
+    this.ApplicationRef.attachView(popupComponentRef.hostView);
+
+    popupComponentRef.instance.closed.subscribe(() => {
+      document.body.removeChild(popup);
+      this.ApplicationRef.detachView(popupComponentRef.hostView);
+    });
+
+    popupComponentRef.instance.message = message;
+
+    document.body.appendChild(popup);
+  }
+
+  showAsElement(message: string) {
+    const popupEl: NgElement & WithProperties<PopupComponent> = document.createElement('popup-element') as any;
+
+    popupEl.addEventListener('closed', () => document.body.removeChild(popupEl));
+
+    popupEl.message = message;
+
+    document.body.appendChild(popupEl);
+  }
+  
+}
